2102a0e9606b7f4b82ee6e6438e22962da189cdb
NelsonBarreraRivas <nelsonbr862@gmail.com>
2023-04-08 14:57:10 -0500

    add redirects

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import EnvironmentPlugin from "vite-plugin-environment"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), EnvironmentPlugin('all')],
-  build: {
-    target: 'es2019',
-  },
+  plugins: [react()],
+  appType: 'spa'
+
 })
